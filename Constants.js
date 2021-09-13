1546f66abd7dcda6139f2c99f524d6f4712ccfe1
chung_heon@hotmail.com <chung_heon@hotmail.com>
2021-09-13 08:55:11 +0800

    Add assets and constants

---
+++ Constants.js
@@ -0,0 +1,47 @@
+const TILE_WIDTH = 50;
+const TILE_HEIGHT = 60;
+const DISCARD_SIZE = 100;
+const IMAGE_TILE_WIDTH = 46;
+const IMAGE_TILE_HEIGHT = 61;
+const ASSETS_DIR = location.pathname + "assets/";
+const IMAGE_NAMES = [
+    'cat.png',
+    'mouse.png',
+    'chicken.png',
+    'centipede.png',
+];
+const TILE_POSITION = [
+    20,
+    75,
+    130,
+    185,
+    240,
+    295,
+    350,
+    405,
+    460,
+    515,
+    570,
+    625,
+    680,
+    735];
+const IMAGE_POSITION_X = [
+    20, 74.5, 129, 183.5, 238, 292.5, 347, 401.5, 456
+]
+
+const IMAGE_POSITION_Y = [
+    17, 83, 149, 215, 281,
+]
+
+export {
+    TILE_WIDTH,
+    TILE_HEIGHT,
+    DISCARD_SIZE,
+    ASSETS_DIR,
+    IMAGE_NAMES,
+    TILE_POSITION,
+    IMAGE_TILE_WIDTH,
+    IMAGE_TILE_HEIGHT,
+    IMAGE_POSITION_X,
+    IMAGE_POSITION_Y
+}
