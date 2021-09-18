cc55cf9bf6820c8360f968ae224d2bc136b80355
chung_heon@hotmail.com <chung_heon@hotmail.com>
2021-09-18 14:23:57 +0800

    change array to dict for hand

--- Models/Hand.js
+++ Models/Hand.js
@@ -12,7 +12,6 @@ export class Hand {
     constructor() {
         this.tiles = {};
         this.tilePositioning = new Array();
-        this.emptyIndex = 0;
     }
 
     //Add tile to hand
@@ -20,82 +19,80 @@ export class Hand {
         //Set position in hand
         tile.x = TILE_POSITION[this.emptyIndex];
         tile.y = CANVAS_HEIGHT - TILE_HEIGHT;
-        
+
         //assign index as tile id
         this.tiles[tile.value] = tile
 
         //Assign tile to empty hand position
-        this.tilePositioning[this.emptyIndex] = tile.value;
-
-        //increment emptyIndex
-        this.emptyIndex++;
+        this.tilePositioning.push(tile.value);
     }
 
     //Discard tile using index from hand position
     discardTile(discardIndex) {
         //Get tile from hand position
         let tileIndex = this.tilePositioning[discardIndex];
-        
+
         //Store discarded tile to return
         let discardedTile = this.tiles[tileIndex];
 
         //Remove the key pair from dictionary tiles
         delete this.tiles[tileIndex];
 
-        //Delete tile from hand and add empty to tail of positioning
-        this.tilePositioning.splice(tileIndex, 1);
-        this.tilePositioning.push(-1);
-
-        //Decrement last know empty index
-        this.emptyIndex--;
+        //Delete tile from hand
+        this.tilePositioning.splice(discardIndex, 1);
 
         return discardedTile;
     }
 
     //Sort the tiles in hand
     sortHand() {
+        const sortedPosition = this.getSorted();
+        this.tilePositioning = sortedPosition;
+    }
+
+    getSorted() {
+        //Create a separate array to contain the sorted position
         let sortedPosition = [];
         for (let count = 0; count < this.tilePositioning.length; count++) {
-            const val = {};
-            if (this.tilePositioning[count] != -1) {
-                val["value"] = this.tiles[this.tilePositioning[count]].value;
-            } else {
-                val["value"] = -1;
-            }
-            val["position"] = this.tilePositioning[count];
+            //Store the value & position for comparison when sorting
+            const val = this.tilePositioning[count];
             sortedPosition.push(val);
         }
-
+        //quick sort the separate array
         quickSort(sortedPosition, 0, sortedPosition.length - 1, this.compareFunc);
-        this.tilePositioning = sortedPosition.map((value) => {
-            if (this.tiles[value] == -1) {
-                return -1;
-            }
-            return value['position'];
-        });
+
+        return sortedPosition;
     }
 
     //for sorting by tile color, and value and special tiles
-    compareFunc(tile, pivot) {
-        const pivotValue = pivot['value'];
-        const tileValue = tile['value'];
+    compareFunc(tileValue, pivotValue) {
         const pivotColor = Math.floor(pivotValue / 100);
         const tileColor = Math.floor(tileValue / 100);
+
+        //if tile == -1 means its bigger as empty tiles go to right
+        //else pivot is bigger
         if (tileValue == -1) {
             return false;
         } else if (pivotValue == -1) {
             return true;
         }
+
+        //Sort by the multiple of 100
         if (tileColor < pivotColor) {
             return true;
         } else if (tileColor == pivotColor) {
+            //if same multiple sort by the tile value
             if (tileColor == 4) {
-                const pVal = Math.floor(pivotValue % 100 / 4);
-                const tVal = Math.floor(tileValue % 100 / 4);
+                //for 4 these tiles are the big tiles
+                //sort by multiples of 4
+                const pVal = Math.floor(pivotValue % 100 / 4 % 7);
+                const tVal = Math.floor(tileValue % 100 / 4 % 7);
                 if (tVal < pVal) {
                     return true;
                 }
             } else {
+                //for any other sort by their value mod 9
+                //since 1-9 for each type
                 const pVal = pivotValue % 100 % 9;
                 const tVal = tileValue % 100 % 9;
                 if (tVal < pVal) {
